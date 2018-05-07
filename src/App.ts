d2844039f6ad979cb04a99b9df83aec0bc89e7ff
Carlos Rubio <thekily@gmail.com>
2018-05-07 19:54:45 +0200

    tslint config fixed tslint errors

--- src/App.ts
+++ src/App.ts
@@ -1,22 +1,22 @@
 import * as express from 'express'
 
 class App {
-  public express
+    public express: express.Express
 
-  constructor () {
-    this.express = express()
-    this.mountRoutes()
-  }
+    constructor() {
+        this.express = express()
+        this.mountRoutes()
+    }
 
-  private mountRoutes (): void {
-    const router = express.Router()
-    router.get('/', (req, res) => {
-      res.json({
-        message: 'Hello World!'
-      })
-    })
-    this.express.use('/', router)
-  }
+    private mountRoutes(): void {
+        const router: express.Router = express.Router()
+        router.get('/', (req: express.Request, res: express.Response) => {
+            res.json({
+                message: 'Hello World!'
+            })
+        })
+        this.express.use('/', router)
+    }
 }
 
 export default new App().express
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import app from './App'
 
-const port = process.env.PORT || 3000
+const port: number = process.env.PORT || 3000
 
-app.listen(port, (err) => {
+app.listen(port, (err: Error) => {
   if (err) {
-    return console.log(err)
+    return console.error(err)
   }
 
   return console.log(`server is listening on ${port}`)
